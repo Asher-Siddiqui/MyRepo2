84d52e2d5811c4a9dffebd32aa0f545fff20c2c9
Asher-Siddiqui <masher.siddiqui@systemsltd.com>
2023-01-17 10:59:37 +0500

    Initial commit

--- app/src/main/java/com/example/practiceapp2/LoginFragment.kt
+++ app/src/main/java/com/example/practiceapp2/LoginFragment.kt
@@ -4,20 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.practiceapp2.databinding.FragmentLoginBinding
+import com.google.firebase.firestore.auth.User
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -41,33 +40,39 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.textView.setOnClickListener{
-            NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_forgotFragment4)
-        }
-    }
+        binding.button1.setOnClickListener {
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            val action = LoginFragmentDirections.actionLoginFragmentToForgotFragment4(binding.editText.text.toString())
+            findNavController().navigate(action)
     }
-
+       }
 }
+
+
+       // binding.button1.setOnClickListener{
+            //NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_forgotFragment4)
+
+            //fun onClick(v: View) {
+           // val myTv: editText = requireView().findViewById(R.id.editText)
+          //  val email= binding.editText.text.toString()
+         //   val action = LoginFragmentDirections.actionLoginFragmentToForgotFragment4(email)
+          //  findNavController().navigate(action)
+
+
+
+
+
+    //    fun onClick(v: View) {
+    //    val amountTv: EditText = requireView().findViewById(R.id.editTextAmount)
+    //    val amount = amountTv.text.toString().toString()
+    //    val action = SpecifyAmountFragmentDirections.confirmationAction(amount)
+    //   v.findNavController().navigate(action)
+    // }
+
+
+
+
+
